640e9664ff2179d66b952850b68bed4cb882bc03
蔡辉 <caihui@rp-pet.cn>
2021-09-18 17:33:51 +0800

    初始化

--- server/ping.go
+++ server/ping.go
@@ -2,13 +2,18 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"server_order/server_common/protobuf/serverorder"
 )
 
 type ServerPing struct {}
 
 func (s ServerPing) Get(ctx context.Context, request *serverorder.GetRequest) (*serverorder.GetResponse, error) {
-	panic("implement me")
+	fmt.Println("Get....")
+
+	return &serverorder.GetResponse{
+		Res:                  "ok",
+	},nil
 }
 
 
