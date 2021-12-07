8458484f813649b4ac80ddb934a2f4cde0c73ed8
V <1156949976@qq.com>
2021-12-07 17:20:04 +0800

    main 优化打印修改

--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"fmt"
-	 "github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
+	grpc_opentracing "github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
 	"google.golang.org/grpc"
 	"log"
 	"net"
@@ -12,41 +11,42 @@ import (
 )
 
 func main()  {
+	var err error
 	var servOpts []grpc.ServerOption
-	var grpcUrl = ":3333"
-
+	grpcUrl := ":9991"
 
 	//服务租约
-	var endpoints = []string{"192.168.59.131:2379"}
-	_, err := comutil.NewServiceRegister(endpoints, "server_order", grpcUrl, 5)
+	_, err = comutil.NewServiceRegister([]string{"192.168.59.131:2379"}, "server_order", grpcUrl, 5)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalf("服务租约启动失败：",err)
 	}
 	//监听续租相应chan
 	//go ser.ListenLeaseRespChan()
 
 	//链路全局化
 	tracer, closer, err := comutil.InitJaeger("server_order","192.168.59.131:6831")
-	if err != nil {
-		panic(fmt.Sprintf("ERROR: cannot init Jaeger: %v\n", err))
+	if err != nil || tracer == nil {
+		log.Fatalf("链路追踪启动失败：",err)
+		return
 	}
 	defer closer.Close()
 	servOpts = append(servOpts,grpc.UnaryInterceptor(grpc_opentracing.UnaryServerInterceptor(grpc_opentracing.WithTracer(tracer))))
 
 	//初始化
 	lis, err := net.Listen("tcp", grpcUrl)
-	if err != nil {log.Fatalf("启动失败: %v", err)}
+	if err != nil {
+		log.Fatalf("服务端口监听失败: %v", err)
+	}
 
-	//新启动rpc服务
+	//新建服务
 	s := grpc.NewServer(servOpts...)
-
 	//服务注入
 	serverorder.RegisterPingServer(s,&server.ServerPing{})
 
-	//服务启动
-	log.Printf(fmt.Sprintf("服务端口启动成功 %s", grpcUrl))
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("当前服务启动失败: %v", err)
+	log.Print("服务运行成功:" + grpcUrl)
+	err = s.Serve(lis)
+	if  err != nil {
+		log.Fatalf("服务运行失败: %v", err)
 	}
 }
 
--- server/ping.go
+++ server/ping.go
@@ -3,34 +3,24 @@ package server
 import (
 	"context"
 	"fmt"
-	"github.com/opentracing/opentracing-go"
 	"server_order/server_common/protobuf/serverorder"
-	"time"
 )
 
 type ServerPing struct {}
 
 func (s ServerPing) Get(ctx context.Context, request *serverorder.GetRequest) (*serverorder.GetResponse, error) {
-	//标签
-	span, _ := opentracing.StartSpanFromContext(ctx, "Get")
-	defer span.Finish()
-	//二级链路
-	Tet(ctx)
+	////标签
+	//span, _ := opentracing.StartSpanFromContext(ctx, "Get")
+	//defer span.Finish()
+	////二级链路
+	//Tet(ctx)
 
-	time.Sleep(time.Second/10)
+	//time.Sleep(time.Second/10)
 
 	fmt.Println("Get....")
 	return &serverorder.GetResponse{
 		Res:                  "ok1111",
 	},nil
-}
-
-func Tet(ctx context.Context)  {
-	//标签
-	span, _ := opentracing.StartSpanFromContext(ctx, "Tet")
-	defer span.Finish()
-
-	time.Sleep(time.Second/30)
 }
 
 
